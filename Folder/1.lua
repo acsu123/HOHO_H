b5d884c12c5117f602e25802039399dc3f482ea0
acsu123 <85284831+acsu123@users.noreply.github.com>
2022-02-26 15:37:43 +0700

    Update 1.lua

--- Folder/1.lua
+++ Folder/1.lua
@@ -1,18 +1,2 @@
 
---[[SCRIPT SETTING]]
-_G.TeamSelect = "Pirate" -- Your team Pirate / Marine
--- turn on 1 hop/time only
--- set all hop value to false for no hop
-_G.HopSetting = {
-    ["weapon"] = "Saber", -- your weapon
-    --Old
-    ["BisentoV2"] = false,
-    ["PinkCoat"] = false,
-    --New
-    ["Swan"] = false,
-    ["DarkBeard"] = false,
-    --Third
-    ["BigMom"] = false,
-    ["rip_indra"] = false,
-}
-loadstring(game:HttpGet("https://raw.githubusercontent.com/acsu123/HOHO_H/HHPAID/BloxFruit/FullPackBloxFud2.lua", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/acsu123/HOHO_H/HHPAID/BloxFruit/BloxFruitPaid.lua", true))()
