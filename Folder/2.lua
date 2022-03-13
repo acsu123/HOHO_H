8b785bbc508875387aea12d6fa6b335067ae6dfe
acsu123 <85284831+acsu123@users.noreply.github.com>
2022-03-13 20:30:08 +0700

    Update 2.lua

--- Folder/2.lua
+++ Folder/2.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/acsu123/HOHO_H/HHPAID/Pet%20Sim%20X/Protected.lua", true))()
+getfenv()['\108\111\97\100\115\116\114\105\110\103'](game:HttpGet("https://raw.githubusercontent.com/acsu123/HOHO_H/HHPAID/Pet%20Sim%20X/Protected.lua", true))()
