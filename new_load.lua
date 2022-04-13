15096f675a0f835bdd700f3a46210f353fd4e895
acsu123 <85284831+acsu123@users.noreply.github.com>
2022-04-13 21:56:48 +0700

    Update new_load.lua

--- new_load.lua
+++ new_load.lua
@@ -1,3 +1,17 @@
 local list = {
-0=
+--bed wars
+Name: BedWars ≡ƒöÑ [DISASTERS + FREE BEEKEEPER!]
+PlaceId: 6872265039
+---
+Name: Game
+PlaceId: 6872274481
+---
+Name: Game Mega
+PlaceId: 8444591321
+---
+Name: Game Micro
+PlaceId: 8560631822
+---
+Done
+
 }
