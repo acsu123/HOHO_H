e71b843544b4a649dcb998308df8a362a7d43516
acsu123 <85284831+acsu123@users.noreply.github.com>
2022-04-14 12:24:36 +0700

    Update new_load.lua

--- new_load.lua
+++ new_load.lua
@@ -124,7 +124,47 @@ local list = {
 }
 
 if list[game.PlaceId] ~= nil then
+  if game.CoreGui:FindFirstChild("Hoho_Hub") then game.CoreGui:FindFirstChild("Hoho_Hub"):Destroy() end
+  wait()
+function pe_Button()
+local ui = Instance.new("ScreenGui")
+ui.Name = "Toggle HOHO"
+ui.Parent = game.CoreGui
+ui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+
+local OPENCLOSE = Instance.new("TextButton")
+OPENCLOSE.Name = "Button"
+OPENCLOSE.Parent = ui
+OPENCLOSE.BackgroundColor3 = Color3.fromRGB(0, 0, 102)
+OPENCLOSE.BorderSizePixel = 0
+OPENCLOSE.Position = UDim2.new(0.120833337, 0, 0.0952890813, 0)
+OPENCLOSE.Size = UDim2.new(0.0447916649, 0, 0.0845824406, 0)
+OPENCLOSE.Text = "Toggle"
+OPENCLOSE.TextColor3 = Color3.fromRGB(255, 255, 255)
+OPENCLOSE.TextScaled = true
+OPENCLOSE.TextSize = 14.000
+OPENCLOSE.TextWrapped = true
+Instance.new("UICorner",OPENCLOSE)
+OPENCLOSE.MouseButton1Click:Connect(function()
+	pcall(function()
+		game.CoreGui:FindFirstChild("Hoho_Hub").Enabled = not game.CoreGui:FindFirstChild("Hoho_Hub").Enabled
+	end)
+end)
+end
   loadstring(list[tonumber(game.PlaceId)])()
+  if workspace:FindFirstChild('.Ignore') then
+	loadstring(game:HttpGet('https://raw.githubusercontent.com/acsu123/HOHO_H/SCRIPTS/elementBattlegr.lua'))()
+end
+repeat wait() until game.CoreGui:FindFirstChild("Hoho_Hub")
+wait(1)
+pe_Button()
+game:GetService("UserInputService").InputBegan:Connect(function(input, chat)
+	if not chat and input.KeyCode == Enum.KeyCode.RightControl then
+		pcall(function()
+			game.CoreGui:FindFirstChild("Hoho_Hub").Enabled = not game.CoreGui:FindFirstChild("Hoho_Hub").Enabled
+		end)
+	end
+end)
 else
   game.Players.LocalPlayer:Kick("game not support")
 end
